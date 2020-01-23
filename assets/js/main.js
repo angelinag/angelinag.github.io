50f98015e2428f74774a1a31a024f827ae6fc3c7
angelinag <andji.gash@gmail.com>
2020-01-23 16:15:05 +0200

    Added modal, cursor, hid scrollbars.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+// TIMELINE
+
+
 var $element=$('.each-event, .title');
 var $window = $(window);
 $window.on('scroll resize', check_for_fade);
@@ -18,3 +21,60 @@ function check_for_fade() {
  
     });
 };
+
+(function($) { "use strict";
+
+//Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px", 
+        t.style.top = n.clientY + "px", 
+        e.style.left = n.clientX + "px", 
+        e.style.top = n.clientY + "px", 
+        i.style.left = n.clientX + "px", 
+        i.style.top = n.clientY + "px"
+    });
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+    function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+    }
+    function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+    }
+    s();
+    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+        o(r[a])
+    }
+    function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+    }
+
+    //modal
+
+    var app = function () {
+        var body = undefined;
+        var menu = undefined;
+        var menuItems = undefined;
+        var init = function init() {
+            body = document.querySelector('body');
+            menu = document.querySelector('.menu-icon');
+            menuItems = document.querySelectorAll('.modal__list-item');
+            applyListeners();
+        };
+        var applyListeners = function applyListeners() {
+            window.addEventListener('load', function () {
+                return setTimeout(function(){
+                    toggleClass(body, 'modal-active');
+                }, 1000);
+            });
+        };
+        var toggleClass = function toggleClass(element, stringClass) {
+            if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+        };
+        init();
+    }();
+
+
+})(jQuery); 
